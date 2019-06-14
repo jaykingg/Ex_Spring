0e8354d1d0370c9035645cbbc080de8baf478149
wisdomose <mojunseo49@naver.com>
2019-06-14 14:33:29 +0900

    페이징 처리 박스 클릭하여 원하는 페이지로 이동. 특정 게시물 이후 리스트로 이동 시 1페이지로 이동하는 현상 -> 진행 중

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -3,12 +3,14 @@ package org.zerock.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.BoardVO;
 import org.zerock.domain.Criteria;
+import org.zerock.domain.PageDTO;
 import org.zerock.service.BoardService;
 
 import lombok.AllArgsConstructor;
@@ -31,7 +33,7 @@ public class BoardController {
 	public void list(Criteria cri, Model model) {
 		log.info("list");
 		model.addAttribute("list", service.getList(cri));
-		
+		model.addAttribute("pageMaker", new PageDTO(cri,123));
 	}
 	
 	@PostMapping("/register")
@@ -54,7 +56,7 @@ public class BoardController {
 	
 	@GetMapping({"/get", "/modify"})
 	/* bno의 명시적 처리  */
-	public void get(@RequestParam("bno") Long bno, Model model) {
+	public void get(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri, Model model) {
 		log.info("/get or modify");
 		model.addAttribute("board", service.get(bno));
 		
@@ -62,21 +64,28 @@ public class BoardController {
 	
 	/*/modify URL로 접근했을 때,함수 내용을 가지고 /board/list 로 리턴한다. */
 	@PostMapping("/modify")
-	public String modify(BoardVO board, RedirectAttributes rttr) {
+	public String modify(BoardVO board, @ModelAttribute("cri") Criteria cri, RedirectAttributes rttr) {
 		log.info("modifyy : " + board);
 		
 		if(service.modify(board)) {
 			rttr.addFlashAttribute("result", "sucess");
 		}
+		
+		rttr.addAttribute("pageNum", cri.getPageNum());
+		rttr.addAttribute("amount", cri.getAmount());
+		
 		return "redirect:/board/list";
 	}
 	
 	@PostMapping("/remove")
-	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+	public String remove(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri, RedirectAttributes rttr) {
 		log.info("remove..." + bno);
 		if(service.remove(bno)) {
 			rttr.addFlashAttribute("result", "success");
 		}
+		
+		rttr.addAttribute("pageNum", cri.getPageNum());
+		rttr.addAttribute("amount", cri.getAmount());
 		
 		return "redirect:/board/list";
 	}
